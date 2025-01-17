255644f50798054c92f8b3edd66cefd7400b8890
misterbluuu <austgiordano@gmail.com>
2025-01-17 16:55:29 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,3 @@
+document.getElementById('toggleTheme').addEventListener('click', function() {
+    alert('To return to my professional site, click the space where the "Creative Portfolio" button was!');
+});
